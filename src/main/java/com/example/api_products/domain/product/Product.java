cbdde537bc68feba4676260d7e51a7d129ada90b
Samuel Maia <153786965+samuelmaia1@users.noreply.github.com>
2024-08-20 13:02:15 -0300

    Criando entidade Product

--- src/main/java/com/example/api_products/domain/product/Product.java
+++ src/main/java/com/example/api_products/domain/product/Product.java
@@ -1,4 +1,25 @@
 package com.example.api_products.domain.product;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Table(name = "products")
+@Entity(name = "products")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+
 public class Product {
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private Integer price_in_cents;
+
+    private Integer stock;
+
+    private Boolean available;
 }
