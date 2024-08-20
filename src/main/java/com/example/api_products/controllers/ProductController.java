f1aeaeff38fc086b910761733a80db4f9d6a86ee
Samuel Maia <153786965+samuelmaia1@users.noreply.github.com>
2024-08-20 12:51:32 -0300

    Criando controller de products

---
+++ src/main/java/com/example/api_products/controllers/ProductController.java
@@ -0,0 +1,17 @@
+package com.example.api_products.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @GetMapping
+    public ResponseEntity getAllProducts(){
+        return ResponseEntity.ok("Servidor funcionando.");
+    }
+
+}
