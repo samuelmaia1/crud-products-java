f7725c5d7ef1808d05464940fd31748c228df6e3
Samuel Maia <153786965+samuelmaia1@users.noreply.github.com>
2024-08-22 09:23:58 -0300

    Adicionando verificações na rota de novo usuário: verificando se já existe algum usuário com o e-mail enviado ou com o login enviado.

--- src/main/java/com/example/api_products/controllers/UserController.java
+++ src/main/java/com/example/api_products/controllers/UserController.java
@@ -4,29 +4,37 @@ package com.example.api_products.controllers;
 import com.example.api_products.domain.user.User;
 import com.example.api_products.domain.user.UserRepository;
 import com.example.api_products.domain.user.UserRequestPost;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
     UserRepository repository;
 
     @PostMapping("/register")
     public ResponseEntity registerUser(@RequestBody @Validated UserRequestPost data){
         try{
-            User user = new User(data);
-            repository.save(user);
-            return ResponseEntity.ok("Usuário criado");
+            if (!repository.existsByEmail(data.email()) && !repository.existsByLogin(data.login())){
+                repository.save(new User(data));
+                return ResponseEntity.ok("Usuário criado com sucesso.");
+            }
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
         catch (Exception e){
             System.out.println(e.getMessage());
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
+    }
+
+    @GetMapping
+    public ResponseEntity getAllUsers(){
+        return ResponseEntity.ok(repository.findAll());
     }
 }
--- src/main/java/com/example/api_products/domain/user/UserRepository.java
+++ src/main/java/com/example/api_products/domain/user/UserRepository.java
@@ -1,6 +1,10 @@
 package com.example.api_products.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    User findByEmail(String email);
+    Boolean existsByEmail(String email);
+    Boolean existsByLogin(String login);
 }
